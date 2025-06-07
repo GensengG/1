946fc51b9bc1b46cf830bda360b642bedfa387cb
GensengG <168531024+GensengG@users.noreply.github.com>
2025-06-07 23:45:39 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { Client } from "./ClientComponents/Client.jsx";
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="clien__body">
+        <Client />
+    </div>
+  );
+}
+
+export default App;
